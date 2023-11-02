b49b8b5982d84c1680e4ba3b511d810cf163f0de
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-11-02 23:40:17 +0530

    writing trading engine

--- src/index.ts
+++ src/index.ts
@@ -121,4 +121,54 @@ app.get("/balances/:userId", (req, res) => {
   res.json({balances: user.balances});
 })
 
+app.get("/quote", (req, res) => {
+  //TODO:Assignmentr
+});
 
+function flipBalance(userId1: string, userId2: string, quantity: number, price: number) {
+  let user1 = users.find(x => x.id === userId1);
+  let user2 = users.find(x => x.id === userId2);
+  if(!user1 || !user2) {
+    return;
+  }
+  user1.balances[TICKER] -= quantity;
+  user2.balances[TICKER] += quantity;
+  user1.balances["USD"] += (quantity * price);
+  user2.balances["USD"] -= (quantity * price);
+}
+
+function fillOrders(side: string, price: number, quantity: number, userId: string): number {
+  let remainingQty = quantity;
+  if (side === "bid") {
+    for (let i = asks.length -1; i >= 0; i--) {
+      if(asks[i].price > price) {
+        continue;
+      }
+      if (asks[i].quantity > remainingQty) {
+        asks[i].quantity -= remainingQty;
+        flipBalance(asks[i].userId, userId, remainingQty, asks[i].price);
+        return 0;
+      }else {
+        remainingQty -= asks[i].quantity;
+        flipBalance(asks[i].userId, userId, asks[i].quantity, asks[i].price);
+        asks.pop();
+      }
+    }
+  }else {
+    for (let i = bids.length -1; i >= 0; i++) {
+      if (bids[i].price < price) {
+        continue;
+      }
+      if (bids[i].quantity > remainingQty) {
+        bids[i].quantity -= remainingQty;
+        flipBalance(userId, bids[i].userId, remainingQty, price);
+        return 0;
+      } else {
+        remainingQty -= bids[i].quantity;
+        flipBalance(userId, bids[i].userId, bids[i].quantity, price);
+        bids.pop();
+      }
+    }
+  }
+  return remainingQty;
+}
