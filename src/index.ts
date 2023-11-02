eca08abf7bc14e10412c2840feb59c6cf20d1b3c
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-11-02 17:52:20 +0530

    adding typescript server

--- src/index.ts
+++ src/index.ts
@@ -29,5 +29,96 @@ const users: User[] = [{
     "USD": 50000
   }
 }, {
+    id: "2",
+    balances: {
+      "GOOGLE": 10,
+      "USD": 50000
+    }
+  }];
 
-  }]
+const bids: Order[] = [];
+const asks: Order[] = [];
+
+//Place a limit order 
+app.post("/order", (req: any, res: any) => {
+  const side: string = req.body.side;
+  const price: number = req.body.price;
+  const quantity: number = req.body.quantity;
+  const userId: string = req.body.userId;
+
+  const remainingQty = fillOrders(side, price, quantity, userId);
+
+  if(remainingQty === 0) {
+    res.json({ filledQuantity: quantity});
+    return;
+  }
+  
+  if (side === "bid") {
+    bids.push({
+      userId,
+      price,
+      quantity:remainingQty
+    });
+    bids.sort((a,b) => a.price < b.price ? -1: 1);
+  } else {
+    asks.push({
+      userId,
+      price,
+      quantity: remainingQty
+    })
+    asks.sort((a,b) => a.price < b.price ? 1 : -1);
+  }
+
+  res.json({
+    filledQuantity: quantity - remainingQty,
+  })
+})
+
+app.get("/depth", (req: any, res: any) => {
+  const depth: {
+    [price: string] : {
+      type: "bid" | "ask",
+      quantity: number,
+    }
+  } = {};
+  
+  for(let i =0 ; i< bids.length; i++) {
+    if(!depth[bids[i].price]){
+      depth[bids[i].price] = {
+        quantity: bids[i].quantity,
+        type: "bid"
+      };
+    }else {
+      depth[bids[i].price].quantity += bids
+      [i].quantity;
+    }
+  }
+
+  for(let i =0; i< asks.length; i++) {
+    if (!depth[asks[i].price]) {
+      depth[asks[i].price] = {
+        quantity: asks[i].quantity,
+        type: "ask"
+      }
+    }else {
+      depth[asks[i].price].quantity += asks[i].quantity;
+    }
+  }
+  res.json({
+    depth
+  })
+})
+
+app.get("/balances/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const user = users.find(x => x.id === userId);
+  if(!user) {
+    return res.json({
+      USD: 0,
+      [TICKER]: 0
+    })
+  }
+  res.json({balances: user.balances});
+})
+
+
